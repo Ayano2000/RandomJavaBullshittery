051820e89ddc8c5f9fa8ccd53b77c0412f7204b9
Arata Yano <aratayano00@gmail.com>
2020-02-04 16:26:57 +0200

    still trying, will check it out at home a bit more

--- Streams/Numbers.java
+++ Streams/Numbers.java
@@ -6,11 +6,11 @@ public class Numbers {
     public static void main(String[] args) {
         // this uses lambdas to print out the digits from 1 too 100
 
-        // IntStream.range(1, 101).forEach(s -> System.out.println(s));
+        IntStream.range(1, 21).forEach(s -> System.out.println(s));
 
         // populate a list with all ints from 1-100 using a lambda and a stream
         List<Integer> list = new ArrayList<>();
-        IntStream.range(1, 401).forEach(num -> list.add(num));
+        IntStream.range(1, 21).forEach(num -> list.add(num));
         // This is weird. I want to use a stream to grab x amount of data from an
         // existing data set, i cant figure out how to do that just yet
         System.out.println("Size " + list.size());
