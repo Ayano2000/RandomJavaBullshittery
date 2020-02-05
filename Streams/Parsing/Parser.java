626f37c1d8f2c679f9b18d8f860daec0f4f9cca0
Arata Yano <aratayano00@gmail.com>
2020-02-05 11:47:54 +0200

    streams are actually pretty cool

--- Streams/Parsing/Parser.java
+++ Streams/Parsing/Parser.java
@@ -17,7 +17,11 @@ public class Parser {
         // males.forEach(System.out::println);
 
         // filter by gender female and sort by age asc
-        // TODO
+        List<Person> sort = people.stream()
+        .filter(person -> person.getGender().equals(Gender.FEMALE))
+        .sorted(Comparator.comparing(Person::getAge))
+        .collect(Collectors.toList());
+        sort.forEach(System.out::println);
     }
 
     private static List<Person> getPeople() {
@@ -28,6 +32,9 @@ public class Parser {
             new Person("Rachel", 25, Gender.FEMALE),
             new Person("Wouter", 19, Gender.MALE),
             new Person("Matthew", 19, Gender.MALE),
+            new Person("Naseemah", 19, Gender.FEMALE),
+            new Person("That Bitch Keziah", 22, Gender.FEMALE),
+            new Person("Rigardt", 23, Gender.MALE),
             new Person("Georgia", 24, Gender.FEMALE)
         );
       }
